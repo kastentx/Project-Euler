28ec0ba518003d9437355c0787f30a6787b2e4ff
KastenTX <kastencode@gmail.com>
2015-06-06 01:30:43 -0700

    Created problem9.cpp

---
+++ problem9.cpp
@@ -0,0 +1,31 @@
+// Nick Kasten
+// Project Euler - Problem #9
+// Special Pythagorean Triplet
+// 6-5-2015
+
+/*-- problem9.cpp ----------------------------------------------------------------
+
+	A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+	
+	a2 + b2 = c2
+	For example, 32 + 42 = 9 + 16 = 25 = 52.
+	
+	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+	Find the product abc.
+
+--------------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+		
+	return 0;
+}
+
+// Function Definitions
+
